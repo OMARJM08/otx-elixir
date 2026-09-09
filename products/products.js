eaf64a3a3ffefa02ab58efdd32f05b3165f32e1a
OMARJM08 <lapmr9015@gmail.com>
2026-09-09 14:40:42 +0300

    Add files via upload

---
+++ products/products.js
@@ -0,0 +1,15 @@
+window.PRODUCTS_DATA = `
+
+name: Emerald Musk
+scent: مسكي منعش - للنساء
+price: 26 د.أ
+---
+name: Purple Heart Diamond
+scent: زهري خشبي - للجنسين
+price: 28 د.أ
+---
+name: Royal Oud
+scent: شرقي عودي - للرجال
+price: 32 د.أ
+
+`;
