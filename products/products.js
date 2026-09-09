bc83411c668f6c817fe3d1b8b4d50645594534ed
OMARJM08 <lapmr9015@gmail.com>
2026-09-09 15:14:49 +0300

    Add files via upload

--- products/products.js
+++ products/products.js
@@ -2,14 +2,42 @@ window.PRODUCTS_DATA = `
 
 name: Emerald Musk
 scent: مسكي منعش - للنساء
-price: 26 د.أ
+price: -- د.أ
 ---
 name: Purple Heart Diamond
 scent: زهري خشبي - للجنسين
-price: 28 د.أ
+price: -- د.أ
 ---
 name: Royal Oud
 scent: شرقي عودي - للرجال
-price: 32 د.أ
+price: -- د.أ
+---
+name: Grey Pearl Diamond
+scent: ؟؟
+price: -- د.أ
+---
+name: Ibraq
+scent: ؟؟
+price: -- د.أ
+---
+name: Pink Diamond Sakura
+scent: ؟؟
+price: -- د.أ
+---
+name: Arrogate Pink
+scent: ؟؟
+price: -- د.أ
+---
+name: Misk
+scent: ؟؟
+price: -- د.أ
+---
+name: Misk Alfajr
+scent: ؟؟
+price: -- د.أ
+---
+name: Nude Coral Diamond
+scent: ؟؟
+price: -- د.أ
 
 `;
